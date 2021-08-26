f90616c4c7386e0ff23c28c308bf260c2e57b7ca
Nikita022-test <73955210+Nikita022-test@users.noreply.github.com>
2021-08-26 23:25:49 +0500

    Update Turtle.java

--- com/turtle/Turtle.java
+++ com/turtle/Turtle.java
@@ -1,1 +1,76 @@
+package com.turtle;
 
+import android.view.*;
+import android.content.*;
+import android.util.*;
+import android.graphics.*;
+import com.didgt.turtle.*;
+
+public abstract class Turtle extends View
+{
+
+	private float x;
+	private float y;
+	private float c;
+	private DictionaryT dic;
+	private Paint p=new Paint();
+	private String l="";
+
+	public Turtle(Context context,AttributeSet attrs){
+		super(context,attrs);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas)
+	{
+		x=0;
+		y=0;
+		c=0;
+		canvas.drawARGB(80,102,204,255);
+		p.setColor(Color.RED);
+		p.setStrokeWidth(10);
+		for(char k:l.toCharArray()){
+			rule(k,canvas);
+		}
+	}
+
+	public void setDis(DictionaryT d){
+		dic=d;
+	}
+
+	public void start(String key,int n ){
+		for(int i=0;i<n;i++){
+			String fkey="";
+			for(char k:key.toCharArray()){
+				fkey+=dic.getRule(k);
+			}
+			key=fkey;
+			fkey="";
+		}
+		l=key;
+		onDraw(new Canvas());
+	}
+
+	protected void leftTurn(float c){
+		this.c-=c;
+	}
+
+	protected void rightTurn(float c){
+		this.c+=c;
+	}
+
+	protected void step(Canvas canvas){
+		float xS=x;
+		float yS=y;
+		bounce();
+		canvas.drawLine(xS,yS,x,y,p);
+	}
+
+	protected void bounce(){
+		x+=Math.cos(Math.toRadians(c))*50;
+		y+=Math.sin(Math.toRadians(c))*50;
+	}
+
+	protected abstract void rule(char key,Canvas canvas);
+
+}
